b04c7d437f1b06dff9733ac2c5ca0d2e5699d133
shaifalivashistha <21f1003257@student.onlinedegree.iitm.ac.in>
2022-11-09 23:16:24 +0530

    updated vue application

--- frontend/myapp/src/main.js
+++ frontend/myapp/src/main.js
@@ -2,7 +2,6 @@ import Vue from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
-import BootstrapVue from "bootstrap-vue";
 import "./registerServiceWorker";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
